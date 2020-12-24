48b8f7001f0c6637eb6c2074026accbeb72194f3
Bone <1549154618@qq.com>
2020-12-24 12:41:10 +0800

    fix

--- README.md
+++ README.md
@@ -4,14 +4,4 @@
 egg 框架的 socket.io 插件
 原作者许久未更新， 将 socket.io 升级到了 3.0
 
-```bash
-io.of(namespace).route(event, callback)
-```
-
-路由的写法改为 =>
-
-```bash
-io.route(event, callback, namespace)
-```
-
 packet 的 middleware 去掉了
--- app/extend/application.js
+++ app/extend/application.js
@@ -1,14 +1,36 @@
 'use strict';
 
-const SocketIO = require('../../lib/socket.io');
+const { Server } = require('socket.io');
 
 const SocketIOSymbol = Symbol.for('EGG-SOCKET.IO#IO');
+
+const RouterConfigSymbol = Symbol.for('EGG-SOCKET.IO#ROUTERCONFIG');
+
+const assert = require('assert');
+const is = require('is-type-of');
 
 module.exports = {
   get io() {
     if (!this[SocketIOSymbol]) {
-      this[SocketIOSymbol] = new SocketIO();
+      this[SocketIOSymbol] = new Server();
       this[SocketIOSymbol].serveClient(false);
+
+      Object.getPrototypeOf(this[SocketIOSymbol]).route = function() {
+        return this.sockets.route.apply(this.sockets, arguments);
+      };
+
+      Object.getPrototypeOf(this[SocketIOSymbol].of('/')).route = function(event, handler) {
+        assert(is.string(event), 'event must be string!');
+
+        if (!this[RouterConfigSymbol]) {
+          this[RouterConfigSymbol] = new Map();
+        }
+
+        if (!this[RouterConfigSymbol].has(event)) {
+          this[RouterConfigSymbol].set(event, handler);
+        }
+      };
+
     }
     return this[SocketIOSymbol];
   },
