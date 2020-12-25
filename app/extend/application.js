576412e33e2b62c6a4fe83ce96bcbb5c70c077f3
Bone <1549154618@qq.com>
2020-12-25 23:17:19 +0800

    fix

--- app/extend/application.js
+++ app/extend/application.js
@@ -1,37 +1,37 @@
-'use strict';
+'use strict'
 
-const { Server } = require('socket.io');
+const { Server } = require('socket.io')
 
-const SocketIOSymbol = Symbol.for('EGG-SOCKET.IO#IO');
+const SocketIOSymbol = Symbol.for('EGG-SOCKET.IO#IO')
 
-const RouterConfigSymbol = Symbol.for('EGG-SOCKET.IO#ROUTERCONFIG');
+const RouterConfigSymbol = Symbol.for('EGG-SOCKET.IO#ROUTERCONFIG')
 
-const assert = require('assert');
-const is = require('is-type-of');
+const assert = require('assert')
+const is = require('is-type-of')
 
 module.exports = {
   get io() {
     if (!this[SocketIOSymbol]) {
-      this[SocketIOSymbol] = new Server();
-      this[SocketIOSymbol].serveClient(false);
+      this[SocketIOSymbol] = new Server()
+      this[SocketIOSymbol].serveClient(false)
 
       Object.getPrototypeOf(this[SocketIOSymbol]).route = function() {
-        return this.sockets.route.apply(this.sockets, arguments);
-      };
+        return this.sockets.route.apply(this.sockets, arguments)
+      }
 
       Object.getPrototypeOf(this[SocketIOSymbol].of('/')).route = function(event, handler) {
-        assert(is.string(event), 'event must be string!');
+        assert(is.string(event), 'event must be string!')
 
         if (!this[RouterConfigSymbol]) {
-          this[RouterConfigSymbol] = new Map();
+          this[RouterConfigSymbol] = new Map()
         }
 
         if (!this[RouterConfigSymbol].has(event)) {
-          this[RouterConfigSymbol].set(event, handler);
+          this[RouterConfigSymbol].set(event, handler)
         }
-      };
+      }
 
     }
-    return this[SocketIOSymbol];
+    return this[SocketIOSymbol]
   },
-};
+}
--- example/app/io/controller/chat.js
+++ example/app/io/controller/chat.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
 module.exports = app => {
   class Controller extends app.Controller {
     async index() {
-      const message = this.ctx.args[0];
-      console.log('chat :', message + ' : ' + process.pid);
-      const say = await this.ctx.service.user.say();
-      this.ctx.socket.emit('res', say);
+      const message = this.ctx.args[0]
+      console.log('chat :', message + ' : ' + process.pid)
+      const say = await this.ctx.service.user.say()
+      this.ctx.socket.emit('res', say)
     }
   }
-  return Controller;
-};
+  return Controller
+}
